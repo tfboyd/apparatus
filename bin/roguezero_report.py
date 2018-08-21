cc4ce82d333c7bd21391b0d8963404a0d77d419c
Victor Bittorf <vbittorf@google.com>
2018-08-21 18:29:13 +0000

    Changing rogue zero to report to prod instance.

--- bin/roguezero_report.py
+++ bin/roguezero_report.py
@@ -6,7 +6,7 @@ import importlib.util
 
 
 
-def report_result(test_id, mlperf_score, platform_type='p3.8xlarge', extras={}, dev=True):
+def report_result(test_id, mlperf_score, platform_type='gcp', extras={}, dev=True):
     os.system('rm -rf benchmark_harness')
     os.system('git clone https://github.com/tfboyd/benchmark_harness.git')
     sys.path.append('./benchmark_harness/oss_bench/upload/')
@@ -29,13 +29,18 @@ def report_result(test_id, mlperf_score, platform_type='p3.8xlarge', extras={}, 
       platform='gcp', platform_type=platform_type)
     test_info = result_info.build_test_info(batch_size=1)
 
+    dataset = 'benchmark_results_dev'
+    if not dev:
+        dataset = 'benchmark_results'
+
     result_upload.upload_result(
       test_result,
       results,
       'google.com:tensorflow-performance',
       test_info=test_info,
       system_info=system_info,
-      extras=extras)
+      extras=extras,
+      dataset=dataset)
 
     os.system('rm -rf benchmark_harness')
 
@@ -51,7 +56,7 @@ def main():
     if result is None:
         # TODO do something better
         result = 0
-    report_result(os.path.basename(output_dir), result)
+    report_result(os.path.basename(output_dir), result, dev=False)
         
 
 if __name__ == '__main__':
