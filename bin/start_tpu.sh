eab1a27bfe978fa27a29ead16f8d8479ad9cd4f5
Victor Bittorf <vbittorf@google.com>
2018-08-30 17:25:30 +0000

    Updaing TPU script.

--- bin/start_tpu.sh
+++ bin/start_tpu.sh
@@ -1,12 +1,13 @@
 
 HOST=`hostname`
 ZONE=`gcloud compute instances list $HOST --format 'csv[no-heading](zone)' 2>/dev/null`
-TF_VERSION=1.9
+#TF_VERSION=1.9
+TF_VERSION='nightly'
 
-TPU_NAME="$HOST_tpu"
+TPU_NAME="test_tpu"
 
 
-echo $HOST $ZONE
+echo TPU NAME: $TPU_NAME
 
 
 gcloud alpha compute tpus create $TPU_NAME --range=10.123.45.0/29 --version=$TF_VERSION --network=default --accelerator-type=v2-8 --zone $ZONE
