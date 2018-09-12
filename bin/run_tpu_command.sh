8e995e63c5a15180a95aa52a303a4dbd5d47c7c2
Victor Bittorf <vbittorf@google.com>
2018-09-12 22:49:30 +0000

    Adding run tpu.

---
+++ bin/run_tpu_command.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+COMMAND_FILE=$1
+BENCHMARK_DIR=$2
+INPUT_DIR=$3
+OUTPUT_DIR=$4
+
+SECONDS=`date +%s`
+HOST=`hostname`
+
+OUTPUT_SUBDIR=$OUTPUT_DIR/${HOST}_${SECONDS}
+
+python3 bin/bake_benchmark.py $COMMAND_FILE $BENCHMARK_DIR $INPUT_DIR $OUTPUT_SUBDIR
+
+
+pushd $BENCHMARK_DIR
+bash main.sh 2>&1 | tee output.txt
+popd
+
+python3 bin/roguezero_report.py $BENCHMARK_DIR/
