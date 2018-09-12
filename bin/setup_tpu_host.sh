bad2e6963fb0fd3c24f7a661df6bd47c5b44498c
Victor Bittorf <vbittorf@google.com>
2018-09-12 23:06:31 +0000

    Debuggin.

--- bin/setup_tpu_host.sh
+++ bin/setup_tpu_host.sh
@@ -5,5 +5,6 @@ set -e
 sudo pip install tf-nightly
 sudo pip install --upgrade oauth2client
 sudo pip install --upgrade google-api-python-client
+sudo pip install google-cloud
 
 pip show google-cloud
