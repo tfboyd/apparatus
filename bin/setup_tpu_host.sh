f0655ae5c2977209169e2a29d2f2874ede7a7a1a
Victor Bittorf <vbittorf@google.com>
2018-09-17 18:04:18 +0000

    Debuggin.

--- bin/setup_tpu_host.sh
+++ bin/setup_tpu_host.sh
@@ -5,7 +5,6 @@ set -e
 sudo pip3 install tf-nightly
 sudo pip3 install --upgrade oauth2client
 sudo pip3 install --upgrade google-api-python-client
-sudo pip install google-cloud
 sudo pip3 install google-cloud
 
 echo 'TPU Host Freeze pip'
--- bin/start_tpu.sh
+++ bin/start_tpu.sh
@@ -1,13 +1,24 @@
 
-HOST=`hostname`
-ZONE=`gcloud compute instances list $HOST --format 'csv[no-heading](zone)' 2>/dev/null`
-#TF_VERSION=1.9
-TF_VERSION='nightly'
-
-TPU_NAME=$1
 
 
-echo TPU NAME: $TPU_NAME
+
+export MLP_TPU_TF_VERSION=nightly
+export MLP_GCP_HOST=`hostname`
+export MLP_GCP_ZONE=`gcloud compute instances list $MLP_GCP_HOST --format 'csv[no-heading](zone)' 2>/dev/null`
+export MLP_TPU_NAME=${MLP_GCP_HOST}__TPU
 
 
-gcloud alpha compute tpus create $TPU_NAME --range=10.123.45.0/29 --version=$TF_VERSION --network=default --accelerator-type=v2-8 --zone $ZONE
+for x in {0..255}; do
+echo gcloud alpha compute tpus create $MLP_TPU_NAME --range=10.255.$x.0/29 --version=$MLP_TPU_TF_VERSION --network=default --accelerator-type=v2-8 --zone $MLP_GCP_ZONE
+gcloud alpha compute tpus create $MLP_TPU_NAME --range=10.255.$x.0/29 --version=$MLP_TPU_TF_VERSION --network=default --accelerator-type=v2-8 --zone $MLP_GCP_ZONE 2>&1 | tee /tmp/create_tpu_log.txt
+
+
+if grep -q "Try a different range." /tmp/create_tpu_log.txt; then
+  # In this case, the network address is taken adn we should re-try this action, incrementing x
+  echo "Trying a different range.";
+else
+  break
+fi
+
+
+done
