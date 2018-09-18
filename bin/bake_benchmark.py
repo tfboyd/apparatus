9011ae573b13dbcd29e029df5a25a0079ddd9d13
Victor Bittorf <vbittorf@google.com>
2018-09-18 00:53:49 +0000

    Debuggin.

--- bin/bake_benchmark.py
+++ bin/bake_benchmark.py
@@ -73,7 +73,7 @@ BENCHMARK_EXIT_CODE=$?
 set -e
 
 echo  gcloud alpha compute tpus delete $MLP_TPU_NAME --zone $MLP_GCP_ZONE
-#yes | gcloud alpha compute tpus delete $MLP_TPU_NAME --zone $MLP_GCP_ZONE
+yes | gcloud alpha compute tpus delete $MLP_TPU_NAME --zone $MLP_GCP_ZONE
 
 exit $BENCHMARK_EXIT_CODE
 ''')
