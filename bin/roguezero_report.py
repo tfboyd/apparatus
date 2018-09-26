c546c10475fe8012aad5c5d394131a0f4c84ea66
Victor Bittorf <vbittorf@google.com>
2018-09-26 04:13:44 +0000

    Debuggin.

--- bin/roguezero_report.py
+++ bin/roguezero_report.py
@@ -7,7 +7,8 @@ import os
 def main():
     os.system('rm -rf /tmp/py3_rogue_env')
     os.system('python3 -m venv /tmp/py3_rogue_env')
-    os.system('. /tmp/py3_rogue_env/bin/activate; sudo pip3 install --upgrade pip')
+    os.system('. /tmp/py3_rogue_env/bin/activate; pip3 install pytz')
+    os.system('. /tmp/py3_rogue_env/bin/activate; pip3 install --upgrade pip')
     
     os.system('. /tmp/py3_rogue_env/bin/activate; pip3 install google-cloud google-cloud-bigquery')
 
@@ -16,7 +17,7 @@ def main():
     dirpath= sys.argv[1]
 
 
-    cmd = '. /tmp/py3_rogue_env/bin/activate; python3 {} {}'.format(helper_path, dirpath)
+    cmd = '. /tmp/py3_rogue_env/bin/activate; python {} {}'.format(helper_path, dirpath)
     print(cmd)
     os.system(cmd)
 
