3484576d9516cbd92b3bef882df9c30b344ac69a
Toby Boyd <tobyboyd@google.com>
2018-09-28 16:26:33 -0700

    Add tests and expanded system info

--- bin/roguezero_report.py
+++ bin/roguezero_report.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-import sys
 import os
+import sys
 
 
 def main():
@@ -9,13 +9,12 @@ def main():
     os.system('python3 -m venv /tmp/py3_rogue_env')
     os.system('. /tmp/py3_rogue_env/bin/activate; pip3 install pytz')
     os.system('. /tmp/py3_rogue_env/bin/activate; pip3 install --upgrade pip')
-    
+
     os.system('. /tmp/py3_rogue_env/bin/activate; pip3 install google-cloud google-cloud-bigquery')
 
     helper_path = sys.argv[0]
-    helper_path = helper_path.replace('rogue', 'helper_rogue')
+    helper_path = helper_path.replace('rogue', 'main_rogue')
     dirpath= sys.argv[1]
-
 
     cmd = '. /tmp/py3_rogue_env/bin/activate; python {} {}'.format(helper_path, dirpath)
     print(cmd)
