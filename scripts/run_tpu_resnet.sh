96962bb7ae5ad2f1ebf55513f1f2551de81b1f0e
Victor Bittorf <vbittorf@google.com>
2018-10-01 20:27:06 +0000

    Debuggin.

--- scripts/run_tpu_resnet.sh
+++ scripts/run_tpu_resnet.sh
@@ -2,10 +2,12 @@
 set -e
 
 export PYTHONPATH=`pwd`/models:$PYTHONPATH
+export PYTHONPATH=`pwd`/tpu/models:$PYTHONPATH
 
 # start timing 
 start=$(date +%s)
 start_fmt=$(date +%Y-%m-%d\ %r)
+
 
 echo "STARTING TIMING RUN AT $start_fmt"
 #python3 tpu/models/official/resnet/resnet_main.py --tpu=$MLP_TPU_NAME --data_dir=$MLP_PATH_GCS_IMAGENET --model_dir=${MLP_GCS_MODEL_DIR} --train_batch_size=1024 --iterations_per_loop=112603 --mode=train --eval_batch_size=1000 --tpu_zone=us-central1-b --num_cores=8 --train_steps=112603
