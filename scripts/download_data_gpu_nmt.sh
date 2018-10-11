8a71ec693a05d984483a8356dd4adcf5c58becda
Victor Bittorf <vbittorf@google.com>
2018-10-11 22:11:37 +0000

    Adding nmt v100x1

--- scripts/download_data_gpu_nmt.sh
+++ scripts/download_data_gpu_nmt.sh
@@ -1,6 +1,6 @@
 set -e
-#gsutil cp -r gs://mlp_resources/benchmark_data/nmt_gpu $1
+gsutil cp -r gs://mlp_resources/benchmark_data/nmt_gpu $1
 
-#ls -lah $1
+ls -lah $1
 
-#mv $1/*/* $1/
+mv $1/*/* $1/
