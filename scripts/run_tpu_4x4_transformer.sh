df19b0fc8ad5f9826833227159a2915067262c70
bitfort <vbittorf@google.com>
2018-10-18 16:17:04 +0000

    Adding Transformer for 4x4s

---
+++ scripts/run_tpu_4x4_transformer.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -e
+
+#--data_dir=$MLP_TRANSFORMER_GCS_DATA/cns/ei-d/home/tpu-perf-team/shibow/transformer/ \
+#--decode_to_file=/cns/el-d/home/tpu-perf-team/shibow/rs=6.3/decode.transformer_mlperf_tpu.translate_ende_wmt32k_packed.new_dataset_lb512_wu16000_test \
+
+
+
+
+DECODE_CMD="t2t-trainer \
+	--data_dir=$MLP_PATH_GCS_TRANSFORMER/data/transformer \
+	--decode_from_file=$MLP_PATH_GCS_TRANSFORMER/wmt14-en-de.src \
+	--decode_hparams=batch_size=64,beam_size=4,alpha=0.6,extra_length=50 \
+	--decode_reference=$MLP_PATH_GCS_TRANSFORMER/wmt14-en-de.ref \
+	--decode_to_file=$MLP_GCS_MODEL_DIR/ \
+	--eval_steps=5 \
+	--hparams=batch_size=512,learning_rate_warmup_steps=16000 \
+	--hparams_set=transformer_mlperf_tpu \
+	--keep_checkpoint_max=10 \
+	--local_eval_frequency=1000 \
+	--model=transformer \
+	--output_dir=$MLP_GCS_MODEL_DIR/eval \
+	--problem=translate_ende_wmt32k_packed \
+	--rpclog=-1 \
+	--schedule=continuous_decode_from_file \
+	--train_steps=250000 \
+  --cloud_tpu_name=$MLP_TPU_NAME"
+
+
+
+CMD="t2t-trainer \
+  --nobinarylog \
+  --data_dir=$MLP_PATH_GCS_TRANSFORMER/data/transformer \
+  --eval_steps=5 \
+  --hparams=learning_rate_warmup_steps=4000 \
+  --hparams_set=transformer_mlperf_tpu \
+  --iterations_per_loop=1000 \
+  --keep_checkpoint_max=10 \
+  --local_eval_frequency=1000 \
+  --cloud_tpu_name=$MLP_TPU_NAME
+  --model=transformer \
+  --output_dir=$MLP_GCS_MODEL_DIR/train \
+  --problem=translate_ende_wmt32k_packed \
+  --rpclog=-1 \
+  --schedule=train \
+  --tpu_num_shards=8 \
+  --train_steps=10000 \
+  --skip_host_call=true \
+  --use_tpu"
+
+
+
+echo $CMD
+
+# start timing 
+start=$(date +%s)
+start_fmt=$(date +%Y-%m-%d\ %r)
+
+$CMD
+
+# end timing
+end=$(date +%s)
+end_fmt=$(date +%Y-%m-%d\ %r)
+echo "ENDING TIMING RUN AT $end_fmt"
+
+
+# report result 
+result=$(( $end - $start )) 
+result_name="transformer"
+
+
+echo "RESULT,$result_name,0,$result,$USER,$start_fmt"
+
