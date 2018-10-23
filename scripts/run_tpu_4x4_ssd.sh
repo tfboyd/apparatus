a282cfd8da02dae5bb9f15075be2f4b098a5b224
bitfort <vbittorf@google.com>
2018-10-23 18:34:53 +0000

    Upading 4x4 ssd params.

--- scripts/run_tpu_4x4_ssd.sh
+++ scripts/run_tpu_4x4_ssd.sh
@@ -21,6 +21,29 @@ start=$(date +%s)
 start_fmt=$(date +%Y-%m-%d\ %r)
 
 
+
+#--hparams=use_bfloat16=true,lr_warmup_steps=18750 \
+#tpu/models/official/retinanet
+
+echo PYTHONPATH $PYTHONPATH
+
+
+# Train Command
+python3 ssd_main.py  --use_tpu=True \
+                     --tpu_name=${MLP_TPU_NAME} \
+                     --device=tpu \
+                     --train_batch_size=1024 \
+                     --training_file_pattern="${MLP_PATH_GCS_SSD}/train-*" \
+                     --resnet_checkpoint=${MLP_GCS_RESNET_CHECKPOINT} \
+                     --model_dir=${MLP_GCS_MODEL_DIR} \
+		     --num_epochs=64 \
+		     --hparams=use_bfloat16=true,lr_warmup_epoch=4.5,weight_decay=8e-4 \
+		     --num_shards=32 \
+		     --iterations_per_loop=625 \
+		     --mode=train &
+
+
+# Evaluation command
 python3 ssd_main.py  --use_tpu=True \
 	--device=tpu \
 	--eval_batch_size=8 \
@@ -32,27 +55,11 @@ python3 ssd_main.py  --use_tpu=True \
         --tpu_name=${MLP_TPU_SIDECAR_NAME} \
 	--train_batch_size=1024 \
 	--val_json_file="${MLP_PATH_GCS_SSD}/instances_val2017.json" \
-	--validation_file_pattern="${MLP_PATH_GCS_SSD}/val-*" &
+	--validation_file_pattern="${MLP_PATH_GCS_SSD}/val-*"
 
+wait
 
-#--hparams=use_bfloat16=true,lr_warmup_steps=18750 \
-#tpu/models/official/retinanet
-
-echo PYTHONPATH $PYTHONPATH
-
-python3 ssd_main.py  --use_tpu=True \
-                     --tpu_name=${MLP_TPU_NAME} \
-                     --device=tpu \
-                     --train_batch_size=1024 \
-                     --training_file_pattern="${MLP_PATH_GCS_SSD}/train-*" \
-                     --resnet_checkpoint=${MLP_GCS_RESNET_CHECKPOINT} \
-                     --model_dir=${MLP_GCS_MODEL_DIR} \
-                     --num_epochs=61 \
-		     --hparams=use_bfloat16=true,lr_warmup_epoch=4.5,weight_decay=8e-4 \
-		     --num_shards=32 \
-		     --iterations_per_loop=1000 \
-		     --mode=train
-
+my_status=$?
 
 # end timing
 end=$(date +%s)
@@ -67,3 +74,4 @@ result_name="ssd"
 
 echo "RESULT,$result_name,0,$result,$USER,$start_fmt"
 
+exit $my_status
