9b396d161c5d803323605be45956b988eebb8b9e
Taylor Robie <taylorrobie@google.com>
2018-10-26 15:07:54 -0700

    debug gcc version

--- scripts/setup_cloud_profiler.sh
+++ scripts/setup_cloud_profiler.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo "Setting up cloud_tpu_profiler"
+
 sudo apt-get update && \
 sudo apt-get install build-essential software-properties-common -y && \
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y && \
@@ -12,6 +14,8 @@ sudo apt-get install gcc-6 g++-6 -y && \
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6 && \
 sudo apt-get install gcc-4.8 g++-4.8 -y && \
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8;
+sudo update-alternatives --auto gcc
+gcc -v
 
 sudo apt-get install python3-pip
 sudo pip3 install -U cloud-tpu-profiler==1.12 google-api-python-client oauth2client
