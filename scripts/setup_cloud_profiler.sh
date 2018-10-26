a796bd732c99aa15ee46f31aafa3c81b731090fc
Taylor Robie <taylorrobie@google.com>
2018-10-26 16:10:16 -0700

    add more c++ apt installs

--- scripts/setup_cloud_profiler.sh
+++ scripts/setup_cloud_profiler.sh
@@ -15,6 +15,7 @@ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /u
 sudo apt-get install gcc-4.8 g++-4.8 -y && \
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8;
 sudo update-alternatives --set gcc /usr/bin/gcc-6
+sudo apt-get upgrade -y libstdc++6
 gcc -v
 
 sudo apt-get install python3-pip
