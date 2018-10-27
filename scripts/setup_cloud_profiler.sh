e0ce22268f74dadad9c222c46af4428b9ece3934
Taylor Robie <taylorrobie@google.com>
2018-10-26 18:29:42 -0700

    refuse to admit defeat under any circumstances

--- scripts/setup_cloud_profiler.sh
+++ scripts/setup_cloud_profiler.sh
@@ -18,6 +18,9 @@ sudo update-alternatives --set gcc /usr/bin/gcc-6
 
 yes | sudo dpkg --configure -a
 sudo apt-get install -f -y
+
+wget http://security.ubuntu.com/ubuntu/pool/main/g/gcc-5/libstdc++6_5.4.0-6ubuntu1~16.04.10_amd64.deb
+sudo dpkg -i libstdc++6_5.4.0-6ubuntu1~16.04.10_amd64.deb 
 # sudo apt-get upgrade -y libstdc++6
 gcc -v
 
