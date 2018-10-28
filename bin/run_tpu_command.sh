43afb075717ab19de8c13a95de6cf8b28460a085
Taylor Robie <taylorrobie@google.com>
2018-10-28 13:29:59 -0700

    make tpu run command more verbose to help debug

--- bin/run_tpu_command.sh
+++ bin/run_tpu_command.sh
@@ -19,9 +19,16 @@ echo "switching to virtual environment"
 source ${RUN_VENV}/bin/activate
 
 pushd $BENCHMARK_DIR
+
+printf "\n\n\nCalling: bootstrap.sh\n"
 bash bootstrap.sh
+
+printf "\n\n\nCalling: setup.sh\n"
 bash setup.sh
+
+printf "\n\n\nCalling: main.sh\n"
 bash main.sh 2>&1 | tee output.txt
 popd
 
+printf "\n\n\nCalling: bin/roguezero_report.py\n"
 python3 bin/roguezero_report.py $BENCHMARK_DIR/
