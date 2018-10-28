3920d5c2e89b6347cfe54c807ba952a45c41cf5b
Taylor Robie <taylorrobie@google.com>
2018-10-28 13:31:46 -0700

    add python-virtualenv install

--- bin/setup_tpu_host.sh
+++ bin/setup_tpu_host.sh
@@ -13,7 +13,7 @@ curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 
 
 sudo apt-get update
-sudo apt-get install -y python3-pip virtualenv
+sudo apt-get install -y python3-pip virtualenv python-virtualenv
 
 which python3
 python3 --version
