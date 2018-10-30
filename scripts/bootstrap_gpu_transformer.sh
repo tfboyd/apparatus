54104699e2ed5ba1a5a4fc880cd6f16341405329
Smit Hinsu <1990079+smit-hinsu@users.noreply.github.com>
2018-10-30 16:14:58 -0700

    Fix transformer model bootstrap commit hash output

--- scripts/bootstrap_gpu_transformer.sh
+++ scripts/bootstrap_gpu_transformer.sh
@@ -5,5 +5,7 @@ set -e
 repo="smit-hinsu/tensor2tensor"
 git clone https://github.com/${repo}.git t2t
 
+cd t2t
 hash=$(git rev-parse HEAD)
-echo "Cloned $repo isat $hash"
+echo "Cloned repo $repo is at $hash"
+cd ..
