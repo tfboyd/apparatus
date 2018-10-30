86192eb9cce1da59415129dcc7f47ea4a59e8472
Smit Hinsu <1990079+smit-hinsu@users.noreply.github.com>
2018-10-30 15:57:46 -0700

    Print commit hash for transformer source repo

--- scripts/bootstrap_gpu_transformer.sh
+++ scripts/bootstrap_gpu_transformer.sh
@@ -2,5 +2,8 @@
 
 set -e
 
-git clone https://github.com/smit-hinsu/tensor2tensor.git t2t
+repo="smit-hinsu/tensor2tensor"
+git clone https://github.com/${repo}.git t2t
 
+hash=$(git rev-parse HEAD)
+echo "Cloned $repo isat $hash"
