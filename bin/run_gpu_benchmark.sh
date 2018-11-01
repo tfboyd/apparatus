dd7f75abe9d5d85bec301214797e4a4e3dc68cc2
Toby Boyd <tobyboyd@google.com>
2018-11-01 16:54:45 -0700

    install mdadm

--- bin/run_gpu_benchmark.sh
+++ bin/run_gpu_benchmark.sh
@@ -1,6 +1,11 @@
 
 set -e
 
+# Install a few libraries needed by the setup scripts
+# Assuming python3/pip is already installed and nvidia-docker2
+apt-get --no-install-recommends install mdadm
+sudo pip3 install pypaml google-api-python-client google-cloud google-cloud-bigquery
+
 CMD_FILE=$1
 DIR=$2
 
